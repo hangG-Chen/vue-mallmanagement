36b610819dbe36a1993233dcdc544a83eb647a72
hangG-Chen <598991760@qq.com>
2020-07-06 12:10:30 +0800

    1.0

--- src/components/user/userlist.vue
+++ src/components/user/userlist.vue
@@ -1,20 +1,25 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-06 11:54:30
- * @LastEditTime: 2020-07-06 11:54:30
+ * @LastEditTime: 2020-07-06 12:06:12
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue-backstage\src\components\user\userlist.vue
 --> 
 <template>
   <div>
-
+    USERLIST
   </div>
 </template>
 
 <script>
   export default {
-    
+    name: 'userlist',
+    data() {
+      return {
+        userdata: {}
+      }
+    }
   }
 </script>
 
